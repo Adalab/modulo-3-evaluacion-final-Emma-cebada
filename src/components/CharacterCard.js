890745ee1e07da20d0426ec32f762ce072d967e9
Emma <emma.cebada@gmail.com>
2020-11-25 11:37:26 +0100

    Add icons alive, dead, unknown, human or alien

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import human from '../images/human.png';
+import alien from '../images/alien.png';
 import '../stylesheets/CharacterCard.scss';
 
 const CharacterCard = (props) => {
@@ -9,6 +11,7 @@ const CharacterCard = (props) => {
         <img className="card__image" src={props.image} alt={props.name} />
         <h2 className="card__title">{props.name}</h2>
         <p className="card__paragraph">{props.species}</p>
+        <img className="card__paragraph-image" src={props.species === "Human" ? human : alien} alt={props.species} title={props.species} />
       </Link>
     </article >
   )
--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import dead from '../images/dead.jpg';
+import unknown from '../images/unknown.png';
+import alive from '../images/alive.png';
+import human from '../images/human.png';
+import alien from '../images/alien.png';
 import '../stylesheets/CharacterDetail.scss';
 
 const CharacterDetail = (props) => {
@@ -13,10 +18,14 @@ const CharacterDetail = (props) => {
         <div className="cardDetail__name">
           <h2 className="cardDetail__name-title">{props.name}</h2>
           <ul className="cardDetail__list">
-            <li className="cardDetail__list-item" >Species: {props.species}.</li>
+            <li className="cardDetail__list-item" >Species: {props.species}.
+            <img className="card__paragraph-image" src={props.species === "Human" ? human : alien} alt={props.species} title={props.species} />
+            </li>
             <li className="cardDetail__list-item" >Planet of origin: {props.origin}.</li>
             <li className="cardDetail__list-item" >Appears in {props.episodes.length} episode(s).</li>
-            <li className="cardDetail__list-item" >Status: {props.status}</li>
+            <li className="cardDetail__list-item" >Status: {props.status}
+              <img className="cardDetail__list-itemIcon" alt={props.status} title={props.status} src={props.status === "Alive" ? alive : props.status === "Dead" ? dead : unknown} />
+            </li>
           </ul>
         </div>
       </article>
